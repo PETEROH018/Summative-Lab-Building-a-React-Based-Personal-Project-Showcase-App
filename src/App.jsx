b1423be3db7b4adc32e40d2c34d32c6f1d52fa6c
PETEROH018 <mathengep63@gmail.com>
2026-05-14 22:10:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <Navbar/>
+   <LandingPage/>
+    </>
+      )
+}
+
+export default App
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+export default function LandingPage(){
+    return(
+        <> 
+        <div>
+            <h3>PETEROH'S SHOES COLLECTION</h3>
+            <p>Welcome to Peteroh's Shop.</p>
+            <p>I will plug you with the latest shoe</p>
+        </div>
+        </>
+       
+    )
+}
