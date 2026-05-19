b1511e278e9df69b1adb37a4544804581852b4b9
PETEROH018 <mathengep63@gmail.com>
2026-05-19 12:57:34 +0300

    fixed errors in the comments

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
 //This is the custom hook that is used to fetch data from db.json using a GET method and save it in a fetchedData state
 //It uses a useEffect() with two dependencies i.e url and refetch
-//When a new URL is passes, data is refetched from db.json
-//When the shoes state changes due to a new shoe being added, a shoe detail being patched or a shoe being deleted, a data refetch is triggered by incrementing refetch value by 1
+//When a new URL is passed, data is refetched from db.json
+//When a new shoe is added through a POST request, a shoe detail is patched through a PATCH request or a shoe is deleted through a DELETE request, a data refetch is triggered by incrementing refetch value by 1
 export default function useFetch(url,refetch){ 
         const[fetchedData,setFetchedData] = useState([])
         useEffect(()=>{
