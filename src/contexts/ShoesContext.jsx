3f7a0c0a66d20159c5788ab22fee2ca18e36eb71
PETEROH018 <mathengep63@gmail.com>
2026-05-19 12:51:08 +0300

    Added comments and restructured the code by adding a refetch trigger which triggers a refetch in useFetch custom hook each time a POST,PUT, PATCH or DELETE request happens instead of using the shoes state to trigger the refetch which was causing an infinite refetch and drastically affecting performance

--- src/contexts/ShoesContext.jsx
+++ src/contexts/ShoesContext.jsx
@@ -1,16 +1,21 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch"
-const ShoesContext = createContext()
 
+const ShoesContext = createContext() //ShoesContext is declared here 
+
+//A ShoesProvider is exported which uses useFetch custom hook to obtain shoes data by passing a json server URL
+//The shoes data is then saved in a shoes state that is updated using a setShoes setter function
 export function ShoesProvider({children}){
       const [shoes,setShoes] = useState([])
-      const fetchedData = useFetch("http://localhost:3000/shoes",shoes)
-      useEffect(()=>{
-            setShoes(fetchedData)
-            },[fetchedData]
+      const [refetch,setRefetch] = useState(0) //This state is used to trigger a refetch every time a POST,PUT,PATCH or DELETE request is made
+      const fetchedData = useFetch("http://localhost:3000/shoes",refetch)
+      console.log(fetchedData)
+      useEffect(()=>{ //This useEffect prevents triggering an infinite re-render by ensuring that the shoes state is only updated when fetchedData changes
+                setShoes(fetchedData)
+               },[fetchedData]
             )
       return (
-        <ShoesContext.Provider value = {{shoes,setShoes}}>
+        <ShoesContext.Provider value = {{shoes,setShoes,setRefetch}}>
             {children}
         </ShoesContext.Provider> 
       )
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react"
-export default function useFetch(url,shoes){
+//This is the custom hook that is used to fetch data from db.json using a GET method and save it in a fetchedData state
+//It uses a useEffect() with two dependencies i.e url and refetch
+//When a new URL is passes, data is refetched from db.json
+//When the shoes state changes due to a new shoe being added, a shoe detail being patched or a shoe being deleted, a data refetch is triggered by incrementing refetch value by 1
+export default function useFetch(url,refetch){ 
         const[fetchedData,setFetchedData] = useState([])
         useEffect(()=>{
                 fetch(url)
-                .then(response => response.json())
+                .then(response => {
+                     if (!response.ok) {
+                        throw new Error (response.status)
+                     }
+                     return response.json()
+                })
                 .then(data => setFetchedData(data))
-            },[url,shoes])
+                .catch(error => alert(`could not fetch data from the server,${error}`))
+            },[url,refetch])
     return fetchedData
 }
